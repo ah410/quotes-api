0f672b2f6012eafb605b54a11f571d6cd0f5a29b
Ali Hamad <115514687+ah410@users.noreply.github.com>
2025-08-26 20:08:56 -0700

    feat: added rest of functionality for graphql api

---
+++ index.js
@@ -0,0 +1,43 @@
+// Followed documentation at https://www.apollographql.com/docs/apollo-server/getting-started/
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import fs from 'fs';
+
+// Load quotes from JSON file
+const quotes = JSON.parse(fs.readFileSync('quotes.json', 'utf-8'));
+
+// GraphQL Schema
+const typeDefs = `
+    type Quote {
+        id: ID!
+        text: String!
+        author: String!
+    }
+
+    type Query {
+        quotes: [Quote!]!
+        quote(id: ID!): Quote
+        randomQuote: Quote
+    }
+`; // ! means non-nullable
+
+// Define how to fetch the data
+const resolvers = {
+    Query: {
+        quotes: () => quotes,
+        quote: (_, { id }) => quotes.find(quote => quote.id === id),
+        randomQuote: () => quotes[Math.floor(Math.random() * quotes.length)]
+    }
+};
+
+// Create Apollo Server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Creates an Express app, makes ApolloServer instance available as middleware, and prepares the app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
